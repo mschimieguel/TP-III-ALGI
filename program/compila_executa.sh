6e209b71b0b56c11ec2eea5d7ce40e17b7a2cee7
mschimieguel <mschimieguel@gmail.com>
2019-06-10 15:14:23 -0300

    entrada

--- program/compila_executa.sh
+++ program/compila_executa.sh
@@ -2,10 +2,18 @@
 cd ../
 ls
 make clean
-make 
-if [ $2 == 1 ];then
+make
+if [ $3 == 1 ];then
 	clear
 	clear
 fi
 cd bin 
-./tp3 "../entradas/$1.txt"
+
+if [ $2 == 1 ];then
+	./tp3 "tarefa1" "../entradas/$trilha1.txt"
+fi
+if [$2 == 2 ];then
+	./tp3 "tarefa2" "../entradas/$trilha1.txt"
+fi
+
+
--- program/main.c
+++ program/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc,char *argv[]){
 	char *tarefa = argv[1]; 
 	FILE *ARQ = fopen(argv[2],"r");
 	if(!ARQ_){
 		printf("Arquivo de Trilhas Inexistente\n");
+		return 0;
+	}
+	if ( strcomp(tarefa,"tarefa1") == 0 ){
+		//realizar tarefa {"Solucao otima","grafo sem ciclos","tempo max 2 segundos"}
+	}
+	else if ( strcomp(tarefa,"tarfefa2") == 0 ){
+		//realizar tarefa2 {"heuristica","solucao ate 2 vezes pior que a otima","grafo pode conter ciclos"}
+	}
+	else {
+		printf("Por favor execute novamente e passe como primeiro parametro tarefa1 ou tarefa2 \n");
 		return 0;
 	}
 }
